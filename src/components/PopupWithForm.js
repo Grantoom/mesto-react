d00b035e91cd2d0901d865200596d7c7349b7693
Maxim Kovalev <wolverin12@ya.ru>
2023-10-07 17:17:45 +0500

    fixed code

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React from 'react';
 
 function PopupWithForm(props) {
- return(
-    <>
-        
-    </>
- )   
+  return (
+    <div className={`popup popup_${props.name}`}>
+      <div className={`popup__container ${props.containerClass}`}>
+        <button className="popup__exit" type="button" aria-label="Закрыть окно"></button>
+        <form className={`popup__form popup__form_${props.name}`} name={props.formName} method={props.method} noValidate>
+          <h2 className="popup__header">{props.title}</h2>
+          {props.children}
+          <button className={`popup__submit-button ${props.buttonClass}`} type="submit">{props.buttonText}</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default PopupWithForm;
