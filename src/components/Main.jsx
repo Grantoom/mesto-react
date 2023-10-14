2985a1d47523cfe437ab1d8b017944d6deee329d
Maxim Kovalev <wolverin12@ya.ru>
2023-10-14 16:35:54 +0500

    Переписал с использованием контекста

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import api from '../utils/Api.js';
 import Card from './Card.jsx';
+import {CurrentUserContext} from '../contexts/CurrentUserContext.js';
 
 function Main(props) {
 
-    const[userName, setUserName] = React.useState('');
-    const[userDescription, setUserDescription] = React.useState('');
-    const[userAvatar, setUserAvatar] = React.useState('');
     const[cards, setCards] = React.useState([]);
+    const currentUser = React.useContext(CurrentUserContext);
 
     React.useEffect(() => {
-        Promise.all([api.getUserInfo(), api.getCards()])
-            .then(([resDataUser, resDataCard]) => {
-                setUserName(resDataUser.name)
-                setUserDescription(resDataUser.about)
-                setUserAvatar(resDataUser.avatar)
+        Promise.all([api.getCards()])
+            .then(([resDataCard]) => {
+                
                 setCards(resDataCard)
             })
             .catch(err => console.log(`Что-то пошло не так: ${err}`))
@@ -24,12 +21,12 @@ function Main(props) {
         <main className="content">
             <section className="profile">
                 <button onClick={props.onEditAvatar} className="profile__avatar-edit-button" type="button" aria-label="Редактировать-аватар"></button>
-                <img src={userAvatar} alt="Фото профиля" className="profile__avatar"  />
+                <img src={currentUser.avatar} alt="Фото профиля" className="profile__avatar"  />
 
                 <div className="profile__info">
-                    <h1 className="profile__section-title">{userName}</h1>
+                    <h1 className="profile__section-title">{currentUser.name}</h1>
                     <button onClick={props.onEditProfile} className="profile__edit-button" type="button" aria-label="Изменить"></button>
-                    <p className="profile__section-subtitle">{userDescription}</p>
+                    <p className="profile__section-subtitle">{currentUser.about}</p>
                 </div>
 
                 <button onClick={props.onAddPhoto} className="profile__add-button" type="button" aria-label="Добавить"></button>
