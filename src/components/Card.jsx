620d6cde233e2b9c4b329798bd88c7260030690d
Maxim Kovalev <wolverin12@ya.ru>
2023-10-20 17:46:58 +0500

    Add likes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import {CurrentUserContext} from '../contexts/CurrentUserContext.js';
 
-function Card(props) {
+function Card({card, name, link, likes, owner, onCardClick, openDelete, onCardLike}) {
+
+  function handleLikeClick() {
+    onCardLike(card);
+  }
   const currentUser = React.useContext(CurrentUserContext);
-  const isOwn = props.owner._id === currentUser._id;
-  const isLiked = props.likes.some(i => i._id === currentUser._id);
-  const cardLikeButtonClassName = ( 
-    `card__like-button ${isLiked && 'card__like-button_active'}` 
-  );; 
+  const isOwn = owner._id === currentUser._id;
+  const isLiked = likes.some(i => i._id === currentUser._id);
+  const cardLikeButtonClassName = `card__like-button ${isLiked ? 'card__like-button_active' : ''}`;
+
   return (
       <div className="element">
-        <img onClick = {() => props.onCardClick({link: props.link, name: props.name})} className="element__img" src={props.link} alt={`На карточке ${props.name}`} />
-        {isOwn && <button onClick={props.openDelete} className="element__trash" type="button" aria-label="Удалить карточку"></button>}
+        <img onClick = {() => onCardClick({link: link, name: name})} className="element__img" src={link} alt={`На карточке ${name}`} />
+        {isOwn && <button onClick={openDelete} className="element__trash" type="button" aria-label="Удалить карточку"></button>}
         <div className="element__description">
-          <h2 className="element__text">{props.name}</h2>
-          {cardLikeButtonClassName && <button className="element__vector" type="button" aria-label="Поставить лайк"></button>}
-          <p className="element__like-count">{props.likes.length}</p>
+          <h2 className="element__text">{name}</h2>
+          {cardLikeButtonClassName && <button onClick={handleLikeClick} className="element__vector" type="button" aria-label="Поставить лайк"></button>}
+          <p className="element__like-count">{likes.length}</p>
         </div>
       </div>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,34 +1,24 @@
 import React from "react";
-import api from '../utils/Api.js';
 import Card from './Card.jsx';
 import {CurrentUserContext} from '../contexts/CurrentUserContext.js';
 
-function Main(props) {
+function Main({onEditProfile, onEditAvatar, onAddPhoto, onCardClick, openDelete, cards, onCardLike}) {
 
-    const[cards, setCards] = React.useState([]);
     const currentUser = React.useContext(CurrentUserContext);
-
-    React.useEffect(() => {
-        api.getCards()
-            .then((resDataCard) => {
-                setCards(resDataCard)
-            })
-            .catch(err => console.log(`Что-то пошло не так: ${err}`))
-    }, [])
 
     return (
         <main className="content">
             <section className="profile">
-                <button onClick={props.onEditAvatar} className="profile__avatar-edit-button" type="button" aria-label="Редактировать-аватар"></button>
+                <button onClick={onEditAvatar} className="profile__avatar-edit-button" type="button" aria-label="Редактировать-аватар"></button>
                 <img src={currentUser.avatar} alt="Фото профиля" className="profile__avatar"  />
 
                 <div className="profile__info">
                     <h1 className="profile__section-title">{currentUser.name}</h1>
-                    <button onClick={props.onEditProfile} className="profile__edit-button" type="button" aria-label="Изменить"></button>
+                    <button onClick={onEditProfile} className="profile__edit-button" type="button" aria-label="Изменить"></button>
                     <p className="profile__section-subtitle">{currentUser.about}</p>
                 </div>
 
-                <button onClick={props.onAddPhoto} className="profile__add-button" type="button" aria-label="Добавить"></button>
+                <button onClick={onAddPhoto} className="profile__add-button" type="button" aria-label="Добавить"></button>
 
             </section>
 
@@ -38,8 +28,9 @@ function Main(props) {
                         <Card
                             key={item._id}
                             {...item}
-                            onCardClick={props.onCardClick}
-                            openDelete={props.openDelete}
+                            onCardClick={onCardClick}
+                            openDelete={openDelete}
+                            onCardLike={onCardLike}
                         />
                     );
                 })}
